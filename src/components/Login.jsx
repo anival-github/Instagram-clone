62c13716bdd440d7b52bf0a469b123d32f01a002
Valentin Anikin <anival-94@mail.ru>
2021-01-23 16:25:03 +0300

    feat: add form element to login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,24 @@
 const Login = () => {
   return (
     <form>
-      <input id="" type="text"/>
+      <label htmlFor="username">
+        Enter username:
+        <input
+          id="username"
+          className="username"
+          type="text"
+          placeholder="username"
+        />
+      </label>
+      <label htmlFor="password">
+        Enter password:
+        <input
+          id="password"
+          className="password"
+          type="text"
+          placeholder="password"
+        />
+      </label>
     </form>
   )
 }
